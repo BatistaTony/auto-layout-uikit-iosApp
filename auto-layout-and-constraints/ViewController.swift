7a00be9c1a90e52a0c4350fa0edce975c2903932
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-16 11:33:32 +0100

    Initial Commit

---
+++ auto-layout-and-constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto-layout-and-constraints
+//
+//  Created by Batista Tone on 16/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
