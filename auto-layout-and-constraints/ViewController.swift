1c25fd7d8608b876fac981570cc61b297cc395e4
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-17 10:09:30 +0100

    chore: setup bg programmatically

--- auto-layout-and-constraints/ViewController.swift
+++ auto-layout-and-constraints/ViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var backgroundImageView  = UIImageView()
+    var buttonView = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setBackground()
+        
+    }
+    
+    
+    func setBackground(){
+      
+        self.view.addSubview(backgroundImageView)
+
+        backgroundImageView.backgroundColor = UIColor.red
+        
+        backgroundImageView.image = UIImage(named:"background.png")
+
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backgroundImageView.contentMode = .scaleToFill
+        
+        view.sendSubviewToBack(backgroundImageView)
+    }
+    
+    
+    func setupButton(){
+        
+        self.view.addSubview(buttonView)
+        
+        buttonView.titleLabel?.text  = "Login"
+        buttonView.frame = CGRect(x: 50, y: 50, width: 200, height: 50)
+        buttonView.backgroundColor = UIColor.green
+        buttonView.tintColor = UIColor.white
+        buttonView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        //buttonView.frame.width = 50
+        
     }
 
 
